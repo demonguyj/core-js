f2638909e1afd1f1824799ff85cb0718a6415a56
Denis Pushkarev <zloirock@zloirock.ru>
2021-04-18 19:35:26 +0700

    remove unnecessary line

--- .eslintrc.js
+++ .eslintrc.js
@@ -1010,7 +1010,6 @@ module.exports = {
         'packages/core-js-builder/**',
         'packages/core-js-compat/**',
         // dev
-        'packages/core-js-compat/src/**',
         'scripts/**',
         'tests/observables/**',
         'tests/promises-aplus/**',
