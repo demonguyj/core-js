e48ec23126ae974bd3238bc508cd01f626131560
Denis Pushkarev <zloirock@zloirock.ru>
2021-05-06 21:49:03 +0300

    split `esnext.observable` to `esnext.observable.{ constructor, from, of }`

--- packages/core-js-compat/src/data.js
+++ packages/core-js-compat/src/data.js
@@ -1478,7 +1478,11 @@ const data = {
   },
   'esnext.object.has-own': {
   },
-  'esnext.observable': {
+  'esnext.observable.constructor': {
+  },
+  'esnext.observable.from': {
+  },
+  'esnext.observable.of': {
   },
   'esnext.promise.try': {
   },
